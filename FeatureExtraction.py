141c744e921765ff7da588089a3d625490bbb5dd
elisa <elisa.nguyen@live.de>
2020-01-15 19:06:05 +0100

    extracted mfcc features from audio

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -4,7 +4,9 @@ Feature extraction
 import scipy.io.wavfile as wav
 import speechpy as sp
 import numpy as np
-
+import os
+import pandas as pd
+import glob
 
 def preemphasize_signal(audio):
     """
@@ -22,7 +24,7 @@ def extract_mfcc(audio, rate):
     Method to extract 39 mfcc features (mfcc, energy, deltas, delta deltas) per frame
     :param rate: sampling rate of audio
     :param audio: audio signal (preemphasized best)
-    :return: 39 mfcc features for each frame
+    :return: 12 mfccs, 1 energy, 12 mfcc deltas, 12 mfcc delta-deltas, 1 delta energy, 1 delta-delta energy
     """
     mfcc_features = sp.feature.mfcc(audio, rate, num_cepstral=12)
     mfe_features = sp.feature.lmfe(audio, rate)#get back 40 log energies for each filter for each frame
@@ -60,8 +62,52 @@ def extract_mfcc(audio, rate):
         mean_delta_delta_energy = mean_delta_delta_energy/len(frame_energy_derivatives)
         energy_delta_features.append(mean_delta_energy)
         energy_delta_delta_features.append(mean_delta_delta_energy)
-
     return mfcc_features, energy_features, mfcc_delta_features, mfcc_delta_delta_features, energy_delta_features, energy_delta_delta_features
+
+
+def extract_mfcc_vectors(directory):
+    """
+    Method to extract the MFCC feature vectors (39 dimensions) for each frame of an audio file and write it to csv
+    :param audiofile: Path to audio file
+    """
+    max_no_frames = 0 #know this to fill up the rest with 0 to have comparable data
+    for filename in glob.glob(os.path.join(directory, '*.wav')):
+        signal, rate = preemphasize_signal(filename)
+        file_mfcc = extract_mfcc(signal, rate)[0]
+        max_no_frames = max(max_no_frames, len(file_mfcc))
+    print(max_no_frames)
+    final_data = []
+    for filename in glob.glob(os.path.join(directory, '*.wav')):
+        signal, rate = preemphasize_signal(filename)
+        file_mfcc, file_energy, file_mfcc_d, file_mfcc_d_d, file_energy_d, file_energy_d_d = extract_mfcc(signal, rate)
+        mfcc, energy, mfcc_d, mfcc_dd, e_d, e_dd = [], [], [], [], [], []
+        for coefficients in file_mfcc:
+            mfcc.extend(coefficients)
+        energy = file_energy
+        for delta_coefficients in file_mfcc_d:
+            mfcc_d.extend(delta_coefficients)
+        for delta_delta_coefficients in file_mfcc_d_d:
+            mfcc_dd.extend(delta_delta_coefficients)
+        e_d.extend(file_energy_d)
+        e_dd.extend(file_energy_d_d)
+        print(len(mfcc_dd))
+        if len(file_mfcc) < max_no_frames:
+            difference = max_no_frames - len(file_mfcc)
+            print(difference)
+            mfcc.extend(np.zeros(difference*12, dtype=int))
+            energy.extend(np.zeros(difference, dtype=int))
+            mfcc_d.extend(np.zeros(difference*12, dtype=int))
+            mfcc_dd.extend(np.zeros(difference*12, dtype=int))
+            e_d.extend(np.zeros(difference, dtype=int))
+            e_dd.extend(np.zeros(difference, dtype=int))
+        print(len(mfcc_dd))
+        end_vector = mfcc + energy + mfcc_d + mfcc_dd + e_d + e_dd
+        print(len(end_vector))
+        final_data.append(end_vector)
+    df = pd.DataFrame(final_data)
+    df.to_csv("Data/mfcc_features.csv")
+
+extract_mfcc_vectors("Data/all_p_no_silence/")
 
 
 def extract_spectrum(audio, rate):
@@ -91,5 +137,5 @@ def extract_power_spectrum(audio, rate):
 s,r = preemphasize_signal("Data/p2_segmented/N_p2_0_1a.wav")
 m, e, md, mdd, ed, edd = extract_mfcc(s, r)
 
-print(len(ed), len(edd))
+print(len(mdd[0]))
 
